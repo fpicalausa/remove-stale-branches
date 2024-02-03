0f2f08a4381886b03cdee72781dc8575d270f352
Francois Picalausa <fpicalausa@gmail.com>
2024-02-03 11:18:40 +0900

    fix: replace tokens in cli as well

--- src/cli.ts
+++ src/cli.ts
@@ -14,7 +14,7 @@ removeStaleBranches(octokit, {
   daysBeforeBranchStale: 60,
   daysBeforeBranchDelete: 7,
   staleCommentMessage:
-    "{author} Your branch [{branchName}]({branchUrl}) hasn't been updated in the last 60 days and is marked as stale. It will be removed in a week.\r\nIf you want to keep this branch around, delete this comment or add new commits to this branch.",
+    "{author} Your branch [{branchName}]({branchUrl}) hasn't been updated in the last {daysBeforeBranchStale} days and is marked as stale. It will be removed in a week.\r\nIf you want to keep this branch around, delete this comment or add new commits to this branch.",
   protectedBranchesRegex: "^(main|master)$",
   operationsPerRun: 10,
   githubToken: process.env.GITHUB_TOKEN || "",
