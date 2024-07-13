fecc6e8135239b449a7b9cbc3811dc01fd5131b8
Francois Picalausa <fpicalausa@gmail.com>
2024-07-13 12:32:46 +0900

    chore: remove .d.ts from dist as they aren't really used for gh actions

--- dist/readBranches.d.ts
+++
@@ -1,5 +0,0 @@
-import { Octokit } from "@octokit/core";
-import { Branch, Repo } from "./types";
-export declare function readBranches(octokit: Octokit, headers: {
-    [key: string]: string;
-}, repo: Repo, organization?: string): AsyncGenerator<Branch>;
--- dist/removeStaleBranches.d.ts
+++
@@ -1,3 +0,0 @@
-import { Octokit } from "@octokit/core";
-import { Params } from "./types";
-export declare function removeStaleBranches(octokit: Octokit, params: Params): Promise<void>;
